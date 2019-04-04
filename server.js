b453f52ad505c077693d5aa57b0c3e8e7a97810d
yarynakosiv <yarynakosiv@yahoo.com>
2019-04-04 13:56:59 +0300

    version3

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+// const jsonServer = require('json-server');
+// const auth = require('json-server-auth');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults({
+//     noCors: true,
+// });
+// server.db = router.db;
+// server.use(auth);
+// server.use(middlewares);
+// server.use(router);
+//
+// server.listen(4000, () => {
+//     console.log('JSON Server is running')
+// });
+//
 
-server.use(middlewares);
-server.use((req, res, next) => {
-    if (isAuthorized(req)) { // add your authorization logic here
-        next() // continue to JSON Server router
-    } else {
-        res.sendStatus(401)
-    }
-});
-server.use(router);
-server.listen(4000, () => {
-    console.log('JSON Server is running')
-});
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+app.use(cors());
+app.use(auth);
+app.use(router);
+
+app.listen(4000);
